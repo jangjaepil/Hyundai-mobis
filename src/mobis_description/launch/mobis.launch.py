bb3f9dc6e797f98d1fb4bcebe31b0563efd4722f
jaepil jang <jaepill9@hanyang.ac.kr>
2023-12-15 14:11:55 +0900

    Big Bang

---
+++ src/mobis_description/launch/mobis.launch.py
@@ -0,0 +1,108 @@
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+# def generate_launch_description():
+#     gazebo = IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource([
+#             os.path.join(get_package_share_directory('gazebo_ros'), 'launch/gazebo.launch.py')
+#         ]),
+#         launch_arguments={'paused': 'true'}.items(),
+#     )
+def generate_launch_description():
+    world_file = PathJoinSubstitution(
+        [FindPackageShare("mobis_description"),
+        "world",
+        "empty.world"],
+    )
+    
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                    launch_arguments = {'pause': 'true', 'world': world_file}.items(),
+             )
+
+    urdf_path = os.path.join(get_package_share_directory('mobis_description'))
+
+    xacro_file = os.path.join(urdf_path, 'urdf', 'mobile_ur5e.urdf')
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[
+            params,
+            {"use_sim_time": True}
+        ]
+    )
+    
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'mobile_ur5e'],
+        output='screen'
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_ur5e_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'ur5e_controller'],
+        output='screen'
+    )
+
+    load_mobile_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'mobile_controller'],
+        output='screen'
+    )
+
+    load_lift_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'lift_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_ur5e_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_mobile_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_lift_controller],
+            )
+        ),
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+    ])
