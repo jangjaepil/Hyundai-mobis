34f05bf1912925a9d5f167ff5af3ff5eb37a6d5e
jaepil jang <jaepill9@hanyang.ac.kr>
2023-12-15 14:16:40 +0900

    add link pose

---
+++ src/IFRA_LinkPose/linkpose_gazebo/launch/linkpose.launch.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import xacro
+import yaml
+
+# LOAD FILE:
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available.
+        return None
+# LOAD YAML:
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available.
+        return None
+    
+# ========== **GENERATE LAUNCH DESCRIPTION** ========== #
+def generate_launch_description():
+    
+    # ***** GAZEBO ***** #   
+    # DECLARE Gazebo WORLD file:
+    linkpose_gazebo = os.path.join(
+        get_package_share_directory('linkpose_gazebo'),
+        'worlds',
+        'linkpose.world')
+    # DECLARE Gazebo LAUNCH file:
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                launch_arguments={'world': linkpose_gazebo}.items(),
+             )
+    
+    # ***** RETURN LAUNCH DESCRIPTION ***** #
+    return LaunchDescription([
+        gazebo, 
+    ])
